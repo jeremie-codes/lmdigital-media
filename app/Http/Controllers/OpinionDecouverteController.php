3b1df9be383f8754cb21afebe6420a3c7707614b
jeremie <jeremie-codes>
2025-05-26 10:44:08 +0100

    last commit

--- app/Http/Controllers/OpinionDecouverteController.php
+++ app/Http/Controllers/OpinionDecouverteController.php
@@ -13,10 +13,10 @@ class OpinionDecouverteController extends Controller
     public function index()
     {
 
-        $opinionDecouvertes = OpinionEtDecouverte::all();
-        $categorie = OpinionEtDecouverte::select('categorie')->distinct()->get();
+        // $opinionDecouvertes = OpinionEtDecouverte::all();
+        // $categorie = OpinionEtDecouverte::select('categorie')->distinct()->get();
 
-        return view('politiques', compact('opinionDecouvertes', 'categorie'));
+        return view('politiques');
     }
 
 }
