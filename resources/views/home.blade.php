f34421a77bb3c1adb44fd6696b8f7f7b0cb67bc8
jeremie <jeremie-codes>
2025-05-28 15:57:37 +0100

    kdkdk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,35 +75,39 @@
             <div class="px-6 py-4 mt-2">
                 <h2 class="text-xl font-bold uppercase mb-4">Actualités</h2>
                      <!-- Blog Item -->
-                @foreach($lastnews as $news)
-                <div class="flex flex-col md:flex-row gap-4 border-b border-gray-300 pb-6 pt-4">
-                    <div class="flex-1">
-                        <h3 class="text-sm font-bold text-blue-600 uppercase">
-                           {{ $news->title }}
-                        </h3>
-                        <div class="flex items-center gap-4 text-sm text-gray-500 my-2">
-                            <span><i class="fa fa-calendar-alt" aria-hidden="true"></i>
-                                {{ \Carbon\Carbon::parse($news->scheduled_at)->format('H:i') }}
-                                {{ \Carbon\Carbon::parse($news->scheduled_at)->translatedFormat(', d F') }}</span>
-                            <span><i class="fa fa-user" aria-hidden="true"></i> {{ Illuminate\Support\Str::limit(strip_tags($news->author->name), 8) }}</span>
-                            <span><i class="fa fa-message" aria-hidden="true"></i> {{ $news->comments->count() }}</span>
-                        </div>
-                        <div class="w-full sm:flex justify-end">
-                            <div class="w-full md:w-100 sm:h-38 overflow-hidden border">
-                                <img src="{{ asset('images/img.jpeg') }}" alt="Blog image" class="w-full h-full object-cover">
+                @forelse($lastnews as $news)
+                    <div class="flex flex-col md:flex-row gap-4 border-b border-gray-300 pb-6 pt-4">
+                        <div class="flex-1">
+                            <h3 class="text-sm font-bold text-blue-600 uppercase">
+                            {{ $news->title }}
+                            </h3>
+                            <div class="flex items-center gap-4 text-sm text-gray-500 my-2">
+                                <span><i class="fa fa-calendar-alt" aria-hidden="true"></i>
+                                    {{ \Carbon\Carbon::parse($news->scheduled_at)->format('H:i') }}
+                                    {{ \Carbon\Carbon::parse($news->scheduled_at)->translatedFormat(', d F') }}</span>
+                                <span><i class="fa fa-user" aria-hidden="true"></i> {{ Illuminate\Support\Str::limit(strip_tags($news->author->name), 8) }}</span>
+                                <span><i class="fa fa-message" aria-hidden="true"></i> {{ $news->comments->count() }}</span>
                             </div>
-                            <div class="px-3 w-full">
-                                <div class="text-sm text-gray-700 text-justify sm:pt-0 pt-4">
-                                     {!! $news->content !!}
-                                </div
-                                >
-                                <a href="{{ route('actualites.show', $news->id) }}" class="text-blue-600 text-sm font-semibold hover:underline mt-2 inline-block">Voir Plus</a>
+                            <div class="w-full sm:flex justify-end">
+                                <div class="w-full md:w-100 sm:h-38 overflow-hidden border">
+                                    <img src="{{ asset('images/img.jpeg') }}" alt="Blog image" class="w-full h-full object-cover">
+                                </div>
+                                <div class="px-3 w-full">
+                                    <div class="text-sm text-gray-700 text-justify sm:pt-0 pt-4">
+                                        {!! $news->content !!}
+                                    </div
+                                    >
+                                    <a href="{{ route('actualites.show', $news->id) }}" class="text-blue-600 text-sm font-semibold hover:underline mt-2 inline-block">Voir Plus</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </div>
-                @endforeach
+                    </div>
+                @empty
+                    <h3 class="text-sm font-bold text-blue-600 uppercase">
+                        Aucune actualité disponible pour le moment.
+                    </h3>
+                @endforelse
 
                 <div class="w-full pt-4 text-right">
                     <a href="#" class="text-blue-600 text-sm font-semibold hover:underline mt-2 inline-block uppercase">
@@ -117,7 +121,7 @@
             <div class="px-6 py-4">
                 <h2 class="text-xl font-bold uppercase mb-4">Programmes Vidéos Publiées</h2>
                 <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    @foreach($lastvideos as $video)
+                    @forelse($lastvideos as $video)
                         <div class="bg-white shadow">
                             <div class="relative flex items-center justify-center">
                                 <img src="{{ asset('images/img.jpeg') }}" class="w-full">
@@ -138,7 +142,12 @@
                                 {!! Illuminate\Support\Str::limit(strip_tags($video->content), 100) !!}
                             </div>
                         </div>
-                    @endforeach
+                    @empty
+                        <h3 class="text-sm font-bold text-blue-600 uppercase">
+                            Aucune actualité disponible pour le moment.
+                        </h3>
+                    @endforelse
+
                 </div>
 
                  <div class="w-full pt-4 text-right">
@@ -190,22 +199,24 @@
                         @endforeach
                     </div>
                     <!-- Slider controls -->
-                    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                            </svg>
-                            <span class="sr-only">Previous</span>
-                        </span>
-                    </button>
-                    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                            </svg>
-                            <span class="sr-only">Next</span>
-                        </span>
-                    </button>
+                    @if ($annonces->count() >1)
+                        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                                </svg>
+                                <span class="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                                <span class="sr-only">Next</span>
+                            </span>
+                        </button>
+                    @endif
                 </div>
 
             </div>
