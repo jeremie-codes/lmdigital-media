82f888501e887543c18338438b803443fa1940b2
jeremie <jeremie-codes>
2025-05-29 10:47:37 +0100

    kdkdk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -168,15 +168,18 @@
         <div class="space-y-4 md:mt-0 mt-4 side">
             <div class="border-b pb-6 border-gray-300">
                 @forelse($sidenews as $news)
-                    <div class="bg-white shadow mb-4">
-                        <div class="h-32 relative">
-                            <img src="{{ asset('storage/' . $news->cover_image ) }}" class="w-full">
+                    <div class="bg-white shadow mb-4 overflow-hidden">
+                        <div class="h-52 relative">
+                            <img src="{{ asset('storage/' . $news->cover_image ) }}" class="min-w-full min-h-full object-cover" alt="News image">
                         </div>
                         <div class="bg-blue-600 text-white px-3 py-2 text-xl font-bold">{{ \Carbon\Carbon::parse($news->scheduled_at)->format('H:i') }}
                              <span class="text-sm ml-2">{{ \Carbon\Carbon::parse($news->scheduled_at)->translatedFormat(', d F') }}</span></div>
                         <div class="p-3 font-semibold text-sm uppercase">{{ $news->title }}</div>
                     </div>
                 @empty
+                 <div class="bg-white shadow mb-4">
+                    {{-- <div class="h-52 relative border">
+                    </div> --}}
                     <div class="text-blue-600 text-sm py-4 text-center">
                        <span> Aucune actualité disponible pour le moment.</span>
                     </div>
