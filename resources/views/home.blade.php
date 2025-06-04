fa77b3ceb46259f60bade88e85e24cb2869c3ae8
jeremie <jeremie-codes>
2025-06-04 11:32:16 +0100

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -308,14 +308,14 @@
         <div class="space-y-4 md:mt-0 mt-4 side">
             <div class="border-b pb-6 border-gray-300">
                 @forelse($sidenews as $news)
-                    <div class="bg-white shadow mb-4">
+                    <a href="{{ route('actualites.show', $news->id) }}" class="bg-white shadow mb-4">
                         <div class="h-52 relative overflow-hidden">
                             <img src="{{ asset('storage/' . $news->cover_image ) }}" class="min-w-full min-h-full object-cover" alt="News image">
                         </div>
                         <div class="bg-blue-600 text-white px-3 py-2 text-xl font-bold">{{ \Carbon\Carbon::parse($news->scheduled_at)->format('H:i') }}
                              <span class="text-sm ml-2">{{ \Carbon\Carbon::parse($news->scheduled_at)->translatedFormat(', d F') }}</span></div>
                         <div class="p-3 font-semibold text-sm uppercase">{{ $news->title }}</div>
-                    </div>
+                    </a>
                 @empty
                  <div class="bg-white shadow mb-4">
                     {{-- <div class="h-52 relative border">
